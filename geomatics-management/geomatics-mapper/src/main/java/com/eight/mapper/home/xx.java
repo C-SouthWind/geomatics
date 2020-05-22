8834acd8209cdc28163c1248d7f16bb9013bcf06
C-SouthWind <62464179+C-SouthWind@users.noreply.github.com>
2020-05-22 19:35:22 +0800

    分模块开发

---
+++ geomatics-management/geomatics-mapper/src/main/java/com/eight/mapper/home/xx.java
@@ -0,0 +1,9 @@
+package com.eight.mapper.home;
+
+/**
+ * @author ：chj
+ * @date ：Created in 2020/5/22 18:31
+ * @params :
+ */
+public class xx {
+}
---
+++ geomatics-management/geomatics-service/src/main/java/com/eight/service/plotting/project/xx.java
@@ -0,0 +1,9 @@
+package com.eight.service.plotting.project;
+
+/**
+ * @author ：chj
+ * @date ：Created in 2020/5/22 18:31
+ * @params :
+ */
+public class xx {
+}
