7d5387842d9b78604a49cfe0f627278efcf5a73e
qq85063550 <56746432+qq85063550@users.noreply.github.com>
2020-05-23 13:40:10 +0800

    MappingunitMapper

---
+++ geomatics-management/geomatics-mapper/src/main/java/com/eight/mapper/home/MappingunitMapper.java
@@ -0,0 +1,14 @@
+package com.eight.mapper.home;
+
+import com.eight.model.home.Mappingunit;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+* @Description:  
+* @Param:  
+* @return:  
+* @Author: hxy 
+* @Date: 2020/5/22 
+*/
+public interface MappingunitMapper extends Mapper<Mappingunit> {
+}
