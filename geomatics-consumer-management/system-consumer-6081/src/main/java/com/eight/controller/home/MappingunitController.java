3f9f74a8851bd28d1d59a0e652d54670d2ca1d96
qq85063550 <56746432+qq85063550@users.noreply.github.com>
2020-05-23 18:10:29 +0800

    MappingunitController

---
+++ geomatics-consumer-management/system-consumer-6081/src/main/java/com/eight/controller/home/MappingunitController.java
@@ -0,0 +1,34 @@
+package com.eight.controller.home;
+
+import com.eight.base.BaseController;
+import com.eight.base.ResultData;
+import com.eight.model.home.Mappingunit;
+import com.eight.model.login.User;
+import com.eight.service.EightService;
+import com.eight.vo.TokenVo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：hxy
+ * @date ：Created in 2020/5/23 15:45
+ * @params :  MappingunitController
+ */
+
+@RestController
+@Api(value = "查询测绘单位",tags = "查询测绘单位接口")
+public class MappingunitController extends BaseController {
+    @Autowired
+    private EightService eightService;
+
+    @GetMapping("/selectMappingunit")
+    @ApiOperation(value = "查询功能", notes = "用户查询测绘单位功能")
+    public ResultData selectMappingunit(Mappingunit mappingunit) {
+        TokenVo tokenVo = eightService.selectMappingunit(mappingunit);
+        return tokenVo.getIfSuccess()?super.selectSuccess(tokenVo.getToken()):super.selectFailed();
+    }
+}
