b0636e918129485b1f4d3016eaa72f28cc4533bc
qq85063550 <56746432+qq85063550@users.noreply.github.com>
2020-05-29 10:33:17 +0800

    测绘项目+测绘成果查询

---
+++ geomatics-management/geomatics-mapper/src/main/java/com/eight/mapper/home/MappingprojectMapper.java
@@ -0,0 +1,8 @@
+package com.eight.mapper.home;
+
+
+import com.eight.model.home.Mappingproject;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface MappingprojectMapper extends Mapper<Mappingproject> {
+}
